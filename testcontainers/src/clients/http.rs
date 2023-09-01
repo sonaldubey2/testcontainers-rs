3c0f83eafe1502dd6447b7cf13bb3c1d5080ada1
sdubey2 <sdubey2@ra.rockwell.com>
2023-09-01 13:23:48 -0500

    switch import

--- testcontainers/src/clients/http.rs
+++ testcontainers/src/clients/http.rs
@@ -16,7 +16,7 @@ use std::{
     fmt, io,
     sync::{Arc, RwLock},
 };
-use docker_credential::DockerCredentials;
+use docker_credential::auth::DockerCredentials;
 /// A testcontainers client that uses HTTP to communicate with the docker daemon.
 ///
 /// This client provides an async-based interface.
